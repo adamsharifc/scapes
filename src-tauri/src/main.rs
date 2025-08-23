5dee12012c044ab9ed2973d08287de4b372b0dc6
Adam Sharif <adamsharifc@gmail.com>
2025-08-24 01:15:51 +0500

    migrate and update dependencies: - tauri `1.60` to tauri `2` - svelte '4` to `5` - rust `1.60` to `1.89`
    **the current state does not compile**

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,28 +1,35 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod metadata_extract;
-mod integrations;
-
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![
-      my_custom_command,
-      scan_all_daws
-    ])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    scapes_lib::run()
 }
 
-#[tauri::command]
-fn my_custom_command() -> String {
-  println!("Hello from Rust!");
-  metadata_extract::main();
-  return "Hello from Rust!".into()
-}
+// // Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[tauri::command]
-fn scan_all_daws() -> String{
-  let result = integrations::scan_all_daws();
-  return result;
-}
+// mod metadata_extract;
+// mod integrations;
+
+// fn main() {
+//   tauri::Builder::default()
+//     .invoke_handler(tauri::generate_handler![
+//       my_custom_command,
+//       scan_all_daws
+//     ])
+//     .run(tauri::generate_context!())
+//     .expect("error while running tauri application");
+// }
+
+// #[tauri::command]
+// fn my_custom_command() -> String {
+//   println!("Hello from Rust!");
+//   metadata_extract::main();
+//   return "Hello from Rust!".into()
+// }
+
+// #[tauri::command]
+// fn scan_all_daws() -> String{
+//   let result = integrations::scan_all_daws();
+//   return result;
+// }
